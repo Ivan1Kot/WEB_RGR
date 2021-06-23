25a65204c1fbaab464c825272153da5eebd2a1e3
SUCCpred <45946205+SUCCpred@users.noreply.github.com>
2021-06-23 22:45:51 +0300

    Добавил везде доставку

--- resources/views/hydrohammer.blade.php
+++ resources/views/hydrohammer.blade.php
@@ -5,9 +5,5 @@
 @section("page-content")
     <h1>Услуга гидромолота</h1>
     <h1>2500₽/час</h1>
-        <footer class="pt-4 my-md-5 pt-md-5 border-top">
-            <div class="row">
-            </div>
-        </footer>
     </div>
 @endsection
