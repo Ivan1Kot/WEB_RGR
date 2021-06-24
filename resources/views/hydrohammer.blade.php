f3028321b3adb8e7febf7117d155453fc2c544ca
SUCCpred <45946205+SUCCpred@users.noreply.github.com>
2021-06-24 12:16:06 +0300

    починил косметические мелочи

--- resources/views/hydrohammer.blade.php
+++ resources/views/hydrohammer.blade.php
@@ -3,7 +3,10 @@
 @section("page-title")Расчет@endsection
 
 @section("page-content")
-    <h1>Услуга гидромолота</h1>
-    <h1>2500₽/час</h1>
+    <h1 style="margin-top: 15vw">Услуга гидромолота</h1>
+    <h1 class="m-5">2500₽/час</h1>
+    <p class="lead">
+        <a href="/payment" class="btn btn-lg btn-secondary">Рассчитать ещё</a>
+    </p>
     </div>
 @endsection
