e79b52a6a9402bdf52a4ae85c1787f6bc66d1493
Ivan1Kot <wocker@i.ua>
2021-06-25 16:17:05 +0300

    Мелкие исправления

--- resources/views/reviews.blade.php
+++ resources/views/reviews.blade.php
@@ -14,25 +14,27 @@
                 </ul>
             </div>
         @endif
-        @foreach($data as $d)
-            <div class="card-deck mb-3">
-                <div class="card mb-4 box-shadow">
-                    <div class="card-header bg-dark text-lg-start">
-                        <h4 class="masthead-brand my-0 font-weight-normal">{{$d['fio']}}</h4>
-                        <h4 class="nav-masthead my-0 font-weight-normal">жопа</h4>
-                    </div>
-                    <div class="card-body bg-dark">
-                        <p class="masthead-brand text-lg-start">{{$d['text']}}</p>
-                        @if(session('isUser') == 0)
-                            <div class="nav-masthead">
-                                <a href="/reviews_editor"><img src="images/edit.png" width="30" height="30"></a>
-                                <img src="images/delete.png" width="30" height="30">
+        @if(!empty($data))
+                @foreach($data as $d)
+                    <div class="card-deck mb-3">
+                        <div class="card mb-4 box-shadow">
+                            <div class="card-header bg-dark text-lg-start">
+                                <h4 class="masthead-brand my-0 font-weight-normal">{{$d['fio']}}</h4>
+                                <h4 class="nav-masthead my-0 font-weight-normal">{{$d['timestamp']}}</h4>
                             </div>
-                        @endif
+                            <div class="card-body bg-dark">
+                                <p class="masthead-brand text-lg-start">{{$d['text']}}</p>
+                                @if(session('isUser') == 0)
+                                    <div class="nav-masthead">
+                                        <a href="/reviews_editor"><img src="images/edit.png" width="30" height="30"></a>
+                                        <img src="images/delete.png" width="30" height="30">
+                                    </div>
+                                @endif
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-        @endforeach
+                @endforeach
+        @endif
             @if(session('succes') == 1)
                 <div class="alert alert-success">
                     Отзыв отправлен
