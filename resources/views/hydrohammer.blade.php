71ff2b170c85a645c4a8d39380be4dd4091c5ee2
SUCCpred <45946205+SUCCpred@users.noreply.github.com>
2021-07-22 21:56:48 +0300

    поправил

--- resources/views/hydrohammer.blade.php
+++ resources/views/hydrohammer.blade.php
@@ -6,7 +6,7 @@
     <h1 class="m-5">Услуга гидромолота</h1>
     <h1 class="m-5">2500₽/час</h1>
     <h4>Для окозания услуги, позвоните по номеру </h4>
-    @if(session('session-data') != null && count(session('session-data')) == 0)
+    @if(session('session-data') == null || count(session('session-data')) == 0)
         <p class="lead">
             <a href="/choise" class="btn btn-lg btn-secondary m-5">Рассчитать ещё</a>
         </p>
